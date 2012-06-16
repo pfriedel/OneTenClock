044adc79f2dcf2974ca61fb13cbf426dd0ccdefa
Patrick Friedel <pfriedel@gmail.com>
2012-06-16 15:18:13 -0500

    Added a . glyph.

--- tinyfont.h
+++ tinyfont.h
@@ -12,6 +12,7 @@
 //prog_uchar letters_57[] PROGMEM = /* 9 */ { 0,1, 0,2, 0,4, 1,0, 1,2, 1,4, 2,1, 2,2, 2,3, 9,9 }; 
 
 // 4 px wide
+prog_uchar letters_46[] PROGMEM = /* . */ { 0,4, 9,9 };
 prog_uchar letters_48[] PROGMEM = /* 0 */ { 0,1, 0,2, 0,3, 1,0, 1,4, 2,0, 2,4, 3,1, 3,2, 3,3, 9,9 }; 
 prog_uchar letters_49[] PROGMEM = /* 1 */ { 0,0, 0,4, 1,0, 1,1, 1,2, 1,3, 1,4, 2,4, 9,9 }; 
 prog_uchar letters_50[] PROGMEM = /* 2 */ { 0,0, 0,3, 0,4, 1,0, 1,2, 1,4, 2,0, 2,2, 2,4, 3,1, 3,4, 9,9 }; 
@@ -55,9 +56,9 @@ prog_uchar letters_89[] PROGMEM = /* Y */ { 0,0, 1,1, 2,2, 2,3, 2,4, 3,1, 4,0, 9
 prog_uchar letters_90[] PROGMEM = /* Z */ { 0,0, 0,3, 0,4, 1,0, 1,2, 1,4, 2,0, 2,1, 2,4, 9,9 }; 
 
 // Basically an ASCII lookup table
-prog_uchar* font[] = { letters_48, letters_49, letters_50, letters_51, letters_52, letters_53, letters_54, letters_55, letters_56, letters_57, letters_58, letters_59, letters_60, 0, 0, 0, 0, letters_65, letters_66, letters_67, letters_68, letters_69, letters_70, letters_71, letters_72, letters_73, letters_74, letters_75, letters_76, letters_77, letters_78, letters_79, letters_80, letters_81, letters_82, letters_83, letters_84, letters_85, letters_86, letters_87, letters_88, letters_89, letters_90 };
+prog_uchar* font[] = { letters_46, 0, letters_48, letters_49, letters_50, letters_51, letters_52, letters_53, letters_54, letters_55, letters_56, letters_57, letters_58, letters_59, letters_60, 0, 0, 0, 0, letters_65, letters_66, letters_67, letters_68, letters_69, letters_70, letters_71, letters_72, letters_73, letters_74, letters_75, letters_76, letters_77, letters_78, letters_79, letters_80, letters_81, letters_82, letters_83, letters_84, letters_85, letters_86, letters_87, letters_88, letters_89, letters_90 };
 
 // Define the beginning and end of your available glyphs
-uint16_t fontMin=48;
+uint16_t fontMin=46;
 uint16_t fontMax=90;
 
